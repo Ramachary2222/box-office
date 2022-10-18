a0a614b10048d22c3867e702d9149003d6ddaf2e
Ramachary2222 <ramachary9110@gmail.com>
2022-10-18 20:15:12 +0530

    added search bar and worked with API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MainPageLayout from '../Components/MainPageLayout'
 
 function Home() {
+
+    const [input, setInput] = useState("");
+
+    const OnInputChange = (ev) => {
+        setInput(ev.target.value);
+    }
+    const OnSearch = (ev) => {
+        fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r => r.json()).then(result => {
+            console.log(result);
+        })
+
+    }
+    const OnKeyDown = (ev) => {
+        if (ev.keyCode === 13) {
+            OnSearch();
+        }
+
+    }
+
     return (
-        <MainPageLayout>Home</MainPageLayout>
+        <MainPageLayout>
+            <input type="text" onChange={OnInputChange} value={input} />
+            <button type='button' onClick={OnSearch} onKeyDown={OnKeyDown}>Search</button>
+        </MainPageLayout>
     )
 }
 
