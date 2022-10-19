5f2e09ef3a72024a6c6f16c6bec500c57fefcc10
Ramachary2222 <ramachary9110@gmail.com>
2022-10-19 19:40:13 +0530

    added preview cards from api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import ActorGrid from '../Components/Actors/ActorGrid';
 import MainPageLayout from '../Components/MainPageLayout';
+import ShowGrid from '../Components/shows/ShowGrid';
 import { apiGet } from "../misc/config";
 
 
@@ -38,11 +40,9 @@ function Home() {
             </div>)
         }
         if (results && results.length > 0) {
-            return results[0].show ? results.map((item) => (
-                <div key={item.show.id}>{item.show.name}</div>
-            )) : results.map((item) => (
-                <div key={item.person.id}>{item.person.name}</div>
-            ))
+            return results[0].show ?
+                <ShowGrid data={results} /> :
+                <ActorGrid data={results} />;
         }
         return null;
     }
